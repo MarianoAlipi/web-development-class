a95c1f9f434f89652732f016373f1d17b76354ad
Mariano Alipi <marianoalipi@gmail.com>
2020-09-10 17:02:14 -0500

    Added some formatting and validation.

--- utils.js
+++ utils.js
@@ -13,36 +13,37 @@ function update_total() {
 }
 
 let remove_item = (element_to_delete) => {
+
     let str = element_to_delete.innerHTML;
     let index = str.indexOf("price: ");
     let price = parseFloat(str.substring(index + 7));
 
-    if (price == NaN) {
-        return;
+    if (!isNaN(price)) {
+        total_price -= price;
+        update_total();
     }
-
-    total_price -= price;
-    update_total();
 
     element_to_delete.remove();
 }
 
 function get_element_li (name, price) {
-    // return `<li class="added-item">name: ${name} price: ${price}  <button class="remove-item">remove</button></li>`
+    
     let template = document.createElement('li');
     template.className = "added-item";
-    template.innerHTML = "name: " + name + " price: " + price + "  <button class=\"remove-item\">remove</button>";
+
+    // If a price is invalid, mark it.
+    if (price < 0 || isNaN(price)) {
+        template.classList.add("invalid-item");
+        price = "<span class='invalid-price'>" + price + " (not counted)</span>";
+    }
+
+    template.innerHTML = "<div class='item-details'>name: <strong>" + name + "</strong> price: " + price + "</div>  <button class=\"remove-item\">remove</button>";
 
     return template;
 }
 
 let add_item_to_list_with_template = () => {
     return (event) => {
-        /*
-        add the item to the list
-        add event listener to the button inside the element just added with the remove_item function
-        add the value to the total
-        */
         
         let name = document.querySelector('#item-name').value.trim();
         let price = document.querySelector('#item-value').value.trim();
@@ -50,16 +51,17 @@ let add_item_to_list_with_template = () => {
         document.querySelector('#item-name').value = "";
         document.querySelector('#item-value').value = "";
 
-        console.log(price);
-        console.log(price < 0);
-
-        if (name === "" || price === "" || price < 0) {
+        // If any of the fields is empty, don't add it.
+        if (name === "" || price === "") {
             return;
         }
 
         document.querySelector('#items-list').appendChild(get_element_li(name, price));
-        total_price += parseFloat(price.trim());
-        update_total();
+
+        if (!isNaN(price) & price >= 0) {
+            total_price += parseFloat(price.trim());
+            update_total();
+        }
 
         let new_item = document.querySelector('#items-list').lastElementChild;
         new_item.querySelector('.remove-item').addEventListener('click', (event) => remove_item(new_item));
