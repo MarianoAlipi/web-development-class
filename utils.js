de6a03ad0bb76ff67f1c22755fe70c024be4cf52
Mariano Alipi <marianoalipi@gmail.com>
2020-09-09 20:20:11 -0500

    Adds the element to the list. Missing behavior.

--- utils.js
+++ utils.js
@@ -1,24 +1,31 @@
 function get_element_li (name, price) {
-  return `<li class="added-item">name: ${name} price: ${price}  <button class="remove-item">remove</button></li>`
+    return `<li class="added-item">name: ${name} price: ${price}  <button class="remove-item">remove</button></li>`
 }
 
 let add_item_to_list_with_template = (template_function) => {
-  return (event) => {
-    /*
-      add the item to the list
-      add event listener to the button inside the element just added with the remove_item function
-      add the value to the total
-    */
-  }
+    return (event) => {
+        /*
+        add the item to the list
+        add event listener to the button inside the element just added with the remove_item function
+        add the value to the total
+        */
+        
+        let name = document.querySelector('#item-name').value;
+        let price = document.querySelector('#item-value').value;
+
+        let template = template_function(name, price);
+        document.getElementById('items-list').innerHTML += template;
+    }
 }
+
 /*
- for removing elements could be this way
-  let element_to_delete = document.querySelector("selector").lastElementChild;
-  element_to_delete.parentNode.removeChild(element_to_delete);
-  or we could use ChildNode.remove()
-  https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/remove
+for removing elements could be this way
+let element_to_delete = document.querySelector("selector").lastElementChild;
+element_to_delete.parentNode.removeChild(element_to_delete);
+or we could use ChildNode.remove()
+https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/remove
 */
 
 let remove_item  = (node_to_remove) => {
-
+    
 }
