80d86483d8102895a0ca5339dffba25b3b4c74f6
Mariano Alipi <marianoalipi@gmail.com>
2020-09-09 21:44:01 -0500

    Added removal of items.

--- utils.js
+++ utils.js
@@ -1,8 +1,26 @@
-function get_element_li (name, price) {
-    return `<li class="added-item">name: ${name} price: ${price}  <button class="remove-item">remove</button></li>`
+/*
+for removing elements could be this way
+let element_to_delete = document.querySelector("selector").lastElementChild;
+element_to_delete.parentNode.removeChild(element_to_delete);
+or we could use ChildNode.remove()
+https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/remove
+*/
+
+let remove_item = (element_to_delete) => {
+    element_to_delete.remove();
+    
 }
 
-let add_item_to_list_with_template = (template_function) => {
+function get_element_li (name, price) {
+    // return `<li class="added-item">name: ${name} price: ${price}  <button class="remove-item">remove</button></li>`
+    let template = document.createElement('li');
+    template.className = "added-item";
+    template.innerHTML = "name: " + name + " price: " + price + "  <button class=\"remove-item\">remove</button>";
+
+    return template;
+}
+
+let add_item_to_list_with_template = () => {
     return (event) => {
         /*
         add the item to the list
@@ -13,19 +31,9 @@ let add_item_to_list_with_template = (template_function) => {
         let name = document.querySelector('#item-name').value;
         let price = document.querySelector('#item-value').value;
 
-        let template = template_function(name, price);
-        document.getElementById('items-list').innerHTML += template;
+        document.querySelector('#items-list').appendChild(get_element_li(name, price));
+
+        let new_item = document.querySelector('#items-list').lastElementChild;
+        new_item.querySelector('.remove-item').addEventListener('click', (event) => remove_item(new_item));
     }
-}
-
-/*
-for removing elements could be this way
-let element_to_delete = document.querySelector("selector").lastElementChild;
-element_to_delete.parentNode.removeChild(element_to_delete);
-or we could use ChildNode.remove()
-https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/remove
-*/
-
-let remove_item  = (node_to_remove) => {
-    
 }
