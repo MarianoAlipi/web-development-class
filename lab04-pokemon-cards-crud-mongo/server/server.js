e27367d56c0cb8f97a922502c785c305ed4b5d69
Mariano Alipi <marianoalipi@gmail.com>
2020-10-13 00:06:21 -0500

    Testing Mongoose.

--- lab04-pokemon-cards-crud-mongo/server/server.js
+++ lab04-pokemon-cards-crud-mongo/server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const axios = require('axios')
 const { response } = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 8080
 let cards = {};
@@ -11,91 +12,124 @@ app.use(express.urlencoded({
   extended: true
 }))
 app.use(cors())
-app.get('/favicon.ico', (req, res) => res.status(204));
 
-// Create card.
-app.post('/create/:cardName,:cardType,:cardDetails', (req, res) => {
+// Connect to MongoDB.
+console.log("Connecting to MongoDB database...");
+mongoose.connect('mongodb://localhost/lab04', {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+// On error...
+db.on('error', console.error.bind(console, 'Connection error:'));
 
-    let cardName = req.params["cardName"];
-    let cardType = req.params["cardType"];
-    let cardDetails = req.params["cardDetails"];
+// On success...
+db.once('open', function() {
+    
+    console.log("Connected to MongoDB database.");
+    
+    const kittySchema = new mongoose.Schema({
+        name: String
+    });
+    
+    kittySchema.methods.speak = function () {
+        const greeting = this.name
+            ? "Meow name is " + this.name
+            : "I don't have a name";
+        console.log(greeting);
+    }
 
-    if (cards[cardName] != null) {
-        console.log("A card with the same name ('" + cardName + "') already exists.");
-        res.send("error:duplicate_card");
-    } else {
-        if (cardType == "pokemon") {
-            console.log("Requesting Pokémon '" + cardName + "' data through API...");
-            axios
+
+    const Kitten = mongoose.model('Kitten', kittySchema);
+
+    const silence = new Kitten({name: 'Silence'});
+    silence.speak();
+    
+    // Routes.
+    // Ignore favicon.
+    app.get('/favicon.ico', (req, res) => res.status(204));
+    // Create card.
+    app.post('/create/:cardName,:cardType,:cardDetails', (req, res) => {
+        
+        let cardName = req.params["cardName"];
+        let cardType = req.params["cardType"];
+        let cardDetails = req.params["cardDetails"];
+        
+        if (cards[cardName] != null) {
+            console.log("A card with the same name ('" + cardName + "') already exists.");
+            res.send("error:duplicate_card");
+        } else {
+            if (cardType == "pokemon") {
+                console.log("Requesting Pokémon '" + cardName + "' data through API...");
+                axios
             .get(`https://pokeapi.co/api/v2/pokemon/${cardName}`) 
             .then(pokemon_response => {
-                    cards[pokemon_response.data.name] = {type: cardType, details: pokemon_response.data};
-                    res.send("success");
-                }).catch(function(error) {
-                    console.log(error);
-                    res.send("error");
-              });
-        } else {
-            cards[cardName] = {name: cardName, type: cardType, details: cardDetails};
-            res.send("success");
+                cards[pokemon_response.data.name] = {type: cardType, details: pokemon_response.data};
+                res.send("success");
+            }).catch(function(error) {
+                console.log(error);
+                res.send("error");
+            });
+            } else {
+                cards[cardName] = {name: cardName, type: cardType, details: cardDetails};
+                res.send("success");
+            }
         }
-    }
-});
+    });
 
-// Get card.
-app.get('/get/:cardName', (req, res) => {
+    // Get card.
+    app.get('/get/:cardName', (req, res) => {
+        
+        let cardName = req.params["cardName"];
+        
+        let card = cards[cardName];
+        
+        if (card != null) {
+            console.log("Sending '" + cardName +"' card data to client...");
+            res.send(card);
+        } else {
+            console.log("A card with that name ('" + cardName + "') does not exist.");
+            res.send("error:card_not_found");
+        }
+        
+    });
 
-    let cardName = req.params["cardName"];
+    // Get all cards.
+    app.get('/getAll', (req, res) => {
+        console.log("Sending all cards to client...");
+        res.send(cards);
+    });
 
-    let card = cards[cardName];
+    // Update card.
+    app.put("/update/:cardName,:cardDetails", (req, res) => {
+        
+        let cardName = req.params["cardName"];
+        let cardDetails = req.params["cardDetails"];
+        
+        if (cards[cardName] != null) {
+            cards[cardName]["details"] = cardDetails;
+            res.send("success");
+        } else {
+            res.send("error:card_not_found");
+        }
+        
+    });
 
-    if (card != null) {
-        console.log("Sending '" + cardName +"' card data to client...");
-        res.send(card);
-    } else {
-        console.log("A card with that name ('" + cardName + "') does not exist.");
-        res.send("error:card_not_found");
-    }
-
-});
-
-// Get all cards.
-app.get('/getAll', (req, res) => {
-    console.log("Sending all cards to client...");
-    res.send(cards);
-});
-
-// Update card.
-app.put("/update/:cardName,:cardDetails", (req, res) => {
-
-    let cardName = req.params["cardName"];
-    let cardDetails = req.params["cardDetails"];
-
-    if (cards[cardName] != null) {
-        cards[cardName]["details"] = cardDetails;
+    // Delete card.
+    app.delete('/delete/:cardName', (req, res) => {
+        
+        let cardName = req.params["cardName"];
+        
+        console.log("Deleting card '" + cardName + "'...");
+        delete cards[cardName];
+        
         res.send("success");
-    } else {
-        res.send("error:card_not_found");
-    }
+    });
 
-});
+    // Delete all cards.
+    app.delete('/deleteAll', (req, res) => {
+        console.log("Deleting all cards...");
+        cards = {};
+        res.send("success");
+    });
 
-// Delete card.
-app.delete('/delete/:cardName', (req, res) => {
-    
-    let cardName = req.params["cardName"];
+    app.listen(port)
 
-    console.log("Deleting card '" + cardName + "'...");
-    delete cards[cardName];
-
-    res.send("success");
-});
-
-// Delete all cards.
-app.delete('/deleteAll', (req, res) => {
-    console.log("Deleting all cards...");
-    cards = {};
-    res.send("success");
-});
-
-app.listen(port)
+}); // end of on successful connection to MongoDB
