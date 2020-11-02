f8d420db96b535507dcfb1d4a19f3765da08352c
Mariano Alipi <marianoalipi@gmail.com>
2020-11-02 16:49:20 -0600

    Updated server.js for 'create' and 'join' requests.

--- exam2-pokemon-cards-game/server/server.js
+++ exam2-pokemon-cards-game/server/server.js
@@ -14,7 +14,7 @@ app.use(cors())
 
 // Connect to MongoDB.
 console.log("Connecting to MongoDB database...");
-mongoose.connect('mongodb://localhost/lab04', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost/exam2-rockPaperScissors', {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 // On error...
 db.on('error', console.error.bind(console, 'Connection error:'));
@@ -24,160 +24,78 @@ db.once('open', function() {
     
     console.log("Connected to MongoDB database.");
 
-    const cardSchema = new mongoose.Schema({
-        name: { type: String, required: true, unique: true},
-        pokemonID: { type: Number },
-        cardType: { type: String, required: true },
-        details: String,
-        pokemonTypes: [String],
-        weight: { type: Number, min: 0 },
-        height: { type: Number, min: 0 },
-        baseExperience: { type: Number, min: 0 }
+    const gameSchema = new mongoose.Schema({
+        // Games should be deleted when the host leaves so game IDs can be reused later.
+        // While a game is active, the ID should be unique.
+        gameID: { type: String, required: true, unique: true },
+        nicknameHost: { type: String, required: true },
+        nicknameGuest: { type: String },
+        hostChoice: String,
+        guestChoice: String
     });
 
-    const Card = mongoose.model('Card', cardSchema);
+    const Game = mongoose.model('Game', gameSchema);
     
     // Routes.
     // Ignore favicon.
     app.get('/favicon.ico', (req, res) => res.status(204));
-    // Create card.
-    app.post('/create/:cardName,:cardType,:cardDetails', async (req, res) => {
+    // Create game.
+    app.post('/create/:nickname', async (req, res) => {
         
-        let newCard = new Card();
+        let newGame = new Game();
 
-        const cardName = req.params["cardName"];
-        const cardType = req.params["cardType"];
-        const cardDetails = req.params["cardDetails"];
+        const nickname = req.params["nickname"];
         
-        newCard.name = cardName;
-        newCard.cardType = cardType;
-        newCard.details = cardDetails;
-
-        const queryRes = await Card.findOne({name: cardName}).exec();
-        console.log("queryRes: " + queryRes);
+        // Generate a valid and unique game ID.
+        let gameID = "";
+        let queryRes = null;
         
-        if (queryRes != null) {
-            console.log("A card with the same name ('" + cardName + "') already exists.");
-            res.send("error:duplicate_card");
-        } else {
-            if (cardType == "pokemon") {
-                console.log("Requesting Pokémon '" + cardName + "' data through API...");
-                axios
-                .get(`https://pokeapi.co/api/v2/pokemon/${cardName}`) 
-                .then(pokemon_response => {
-                    
-                    const data = pokemon_response.data;
+        do {
+            gameID = Math.floor(Math.random() * 9999).toString().padStart(4, "0");
+            queryRes = await Game.findOne({gameID}).exec();
+        } while (queryRes != null);
+        
+        newGame.gameID = gameID;
+        newGame.nicknameHost = nickname;
+        newGame.nicknameGuest = null;
+        newGame.hostChoice = null;
+        newGame.guestChoice = null;
 
-                    newCard.pokemonID = data.id;
-
-                    let types = [];
-                    data.types.forEach(element => types.push(element.type.name[0].toUpperCase() + element.type.name.slice(1)));
-                    newCard.pokemonTypes = types;
-
-                    newCard.weight = data.weight / 10; // Converted to kg.
-                    newCard.height = data.height * 10; // Converted to cm.
-                    newCard.baseExperience = data.base_experience;
-
-                    // Save the card.
-                    newCard.save(function (err, result) {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            console.log(result);
-                        }
-                    });
-
-                    res.send("success");
-                }).catch(function(error) {
-                    console.log(error);
-                    res.send("error");
-                });
+        console.log("Creating game with ID " + gameID + "...");
+        
+        // Save the game.
+        newGame.save(function (err, result) {
+            if (err) {
+                console.log(err);
             } else {
-                // Save the card.
-                newCard.save(function (err, result) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log(result);
-                    }
-                });
-                res.send("success");
+                console.log(result);
             }
-        }
+        });
+
+        res.send(gameID);
     });
 
-    // Get card.
-    app.get('/get/:cardName', async (req, res) => {
+    // Join game.
+    app.get('/join/:gameID,:nickname', async (req, res) => {
         
-        const cardName = req.params["cardName"];
-        const queryRes = await Card.findOne({name: cardName}).exec();
+        const gameID = req.params["gameID"];
+        const nickname = req.params["nickname"];
 
-        if (queryRes != null) {
-            console.log("Sending '" + cardName +"' card data to client...");
-            res.send(queryRes);
+        const game = await Game.findOne({gameID}).exec();
+
+        if (game != null) {
+            
+            game.nicknameGuest = nickname;
+            game.save();
+
+            res.send(game);
+
         } else {
-            console.log("A card with that name ('" + cardName + "') does not exist.");
-            res.send("error:card_not_found");
+            console.log("The game does not exist.");
+            res.status(404);
+            res.send("error:game_does_not_exist");
         }
         
-    });
-
-    // Get all cards.
-    app.get('/getAll', async (req, res) => {
-
-        console.log("Sending all cards to client...");
-        
-        const cards = await Card.find({});
-        res.send(cards);
-    });
-
-    // Update card.
-    app.put("/update/:cardName,:cardDetails", async (req, res) => {
-        
-        let cardName = req.params["cardName"];
-        let cardDetails = req.params["cardDetails"];
-        
-        await Card.updateOne({name: cardName}, {details: cardDetails},
-            function (err, res2) {
-                if (err) {
-                    console.log(err);
-                    res.send("error:card_not_found");
-                } else {
-                    console.log(res2);
-                    res.send("success");
-                }
-            }
-        );        
-    });
-
-    // Delete card.
-    app.delete('/delete/:cardName', async (req, res) => {
-        
-        let cardName = req.params["cardName"];
-        
-        console.log("Deleting card '" + cardName + "'...");
-
-        await Card.deleteOne({name: cardName}, function (err) {
-            if (err) {
-                console.log(err);
-                res.send("error");
-            }
-        });
-        
-        res.send("success");
-    });
-
-    // Delete all cards.
-    app.delete('/deleteAll', async (req, res) => {
-
-        console.log("Deleting all cards...");
-
-        await Card.deleteMany({}, function (err) {
-            if (err) {
-                console.log(err);
-            }
-        });
-        res.send("success");
     });
 
     app.listen(port)
